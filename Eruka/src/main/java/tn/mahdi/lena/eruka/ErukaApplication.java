d47e9cde1e9bc5f97b9bf8cec283afe5db29134a
lmahdy <mahdi.mzoughi@istic.ucar.tn>
2026-02-10 17:48:26 +0100

    initial commit

---
+++ Eruka/src/main/java/tn/mahdi/lena/eruka/ErukaApplication.java
@@ -0,0 +1,13 @@
+package tn.mahdi.lena.eruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErukaApplication.class, args);
+	}
+
+}
---
+++ Eruka/src/test/java/tn/mahdi/lena/eruka/ErukaApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.mahdi.lena.eruka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
